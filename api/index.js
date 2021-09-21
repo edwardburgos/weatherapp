8b8673c41ee34b756336e64df210c89bb5a931f0
EdwardBurgos <edwardpbn@gmail.com>
2021-09-20 20:31:19 -0500

    new file of cities without repeats | synchronous server sync

--- api/index.js
+++ api/index.js
@@ -11,53 +11,49 @@ conn.sync({ force: false }).then(async () => {
       const countries = fs.readFileSync('./src/extras/countries.js', 'utf8')
       await Country.bulkCreate(JSON.parse(countries).map(e => { return { nameLower: e.name.toLowerCase(), nameNormal: e.name, code: e.code, codeBig: e.code3 } }));
 
+      console.log('Countries created')
+      
       // Insert states
-      const states = fs.readFileSync('./src/extras/countryStates.js', 'utf-8')
-      await new Promise((resolve, reject) => {
-        JSON.parse(states).forEach(async (e, index) => {
-          try {
-            const country = await Country.findOne({ where: { codeBig: e.iso3 } })
-            if (country) {
-              const countryId = country.id;
-              await new Promise((resolve, reject) => {
-                e.states.forEach(async (e, index) => {
-                  try {
-                    await State.create({ nameLower: e.name.toLowerCase(), nameNormal: e.name, code: e.state_code, countryId: countryId })
-                    if (index === e.states.length - 1) resolve();
-                  }
-                  catch (e) {
-                    console.log(e)
-                  }
-                });
-              });
-            }
-            if (index === JSON.parse(states).length - 1) resolve();
-          } catch (e) {
-            console.log(e)
+      const states = JSON.parse(fs.readFileSync('./src/extras/countryStates.js', 'utf-8'))
+      for (const firstE of states) {
+        try {
+          const country = await Country.findOne({ where: { codeBig: firstE.iso3 } })
+          if (country) {
+            const countryId = country.id;
+            for (const e of firstE.states) {
+              await State.create({ nameLower: e.name.toLowerCase(), nameNormal: e.name, code: e.state_code, countryId: countryId })
+            }     
           }
-        });
-      });
+        } catch (e) {
+          console.log(e)
+        }
+      }
+
+      console.log('States created')
 
       // Insert cities
-      const cities = fs.readFileSync('./src/extras/countryStateCities.js', 'utf-8')
-      await new Promise((resolve, reject) => {
-        JSON.parse(cities).forEach(async e => {
-          try {
-            const country = await Country.findOne({ where: { code: e.country } })
-            let state = ''
-            if (e.state) { state = await State.findOne({ where: { code: e.state, countryId: country.id } }) }
-            await City.create({ nameLower: e.name.toLowerCase(), nameNormal: e.name, stateId: state ? state.id : null, countryId: country.id })
-            if (index === JSON.parse(cities).length - 1) resolve();
-          } catch (err) {
-            console.log(err)
-          }
-        });
-      });
+      const cities = JSON.parse(fs.readFileSync('./src/extras/countryStateCities.js', 'utf-8'))
+      for (const e of cities) {
+        try {
+          const country = await Country.findOne({ where: { code: e.country } })
+          let state = ''
+          if (e.state) { state = await State.findOne({ where: { code: e.state, countryId: country.id } }) }
+          await City.create({ nameLower: e.name.toLowerCase(), nameNormal: e.name, stateId: state ? state.id : null, countryId: country.id })
+        } catch (err) {
+          console.log(err)
+        }
+      }
+      
+      console.log('Cities created')
+
     } catch (e) {
       console.log('Se produjo este error al cargar la base de datos', e)
     }
   }
+
+  console.log('Database created')
+
   server.listen(3001, () => {
-    console.log('%s listening at 3001');
+    console.log('Server listening at 3001');
   });
 });
